8ce097a5a657fd5f7fbc7f5408aef49fe916fc40
Ryan Newton <rrnewton@gmail.com>
2016-09-14 01:19:06 -0400

    Add a 3rd version that unboxes the entitity vector. This gets 1.046ms/tick, 430ms init, and 99% productivity at -A20M (80% at default 500K).
    That's on my laptop, XPS13 Intel i7-6560U CPU @ 2.20GHz.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@ module Main where
 
 -- import NaiveGame0
 -- import NaiveGame1
-import NaiveGame2
+-- import NaiveGame2
+import NaiveGame3
 
 main :: IO ()
 main = run
---
+++ src/NaiveGame3.hs
@@ -0,0 +1,176 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell, RankNTypes, TypeFamilies, MultiParamTypeClasses, RecordWildCards #-}
+
+module NaiveGame3 (run, Block(..), Entity(..), entityName, entitySpeed, Chunk(..), World(..)) where
+
+import Control.DeepSeq
+import Control.Exception (evaluate)
+import GHC.Generics (Generic)
+import Linear
+import qualified Data.Vector.Generic         as G
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as VB
+import Control.Monad.State.Strict
+import Data.Bits
+import Data.Word
+import Data.Int
+import Criterion
+import Criterion.Types
+import Criterion.Main
+import System.Clock
+
+import Data.Vector.Unboxed.Base (Unbox)
+import Data.Vector.Unboxed.Deriving
+    
+data Block = Block { bLocation   :: !(V3 Float)
+                   , bName       :: !String
+                   , bDurability :: !Int
+                   , bTextureId  :: !Int
+                   , bBreakable  :: !Bool
+                   , bVisible    :: !Bool
+                   , bType       :: !Int
+                   } deriving (Eq, Generic, Show)
+
+instance NFData Block
+
+mkBlock :: Int -> Block
+mkBlock n = Block (V3 p p p) ("Block: " ++ show n) 100 1 True True 1
+    where
+        p = fromIntegral n
+
+data EntityType = Zombie
+                | Chicken
+                | Exploder
+                | TallCreepyThing
+                deriving (Eq, Show, Generic, Enum)
+
+data Entity = Entity { eLocation :: !(V3 Float)
+                     , eHealth   :: !Int
+                     , eType     :: !EntityType
+                     } deriving (Eq, Generic, Show)
+            
+instance NFData EntityType
+instance NFData Entity
+
+derivingUnbox "Entity"
+    [t| Entity -> (Float,Float,Float, Int, EntityType) |]
+    [| \ Entity{eLocation=V3 x y z, ..} -> (x,y,z,eHealth,eType) |]
+    [| \ (x,y,z,h,ty) -> Entity (V3 x y z) h ty |]
+
+derivingUnbox "EntityType"
+    [t| EntityType -> Int |]
+    [| fromEnum |]
+    [| toEnum |]
+
+    
+entityName :: EntityType -> String
+entityName e = case e of
+                   TallCreepyThing -> "Tall Creepy Thing"
+                   _ -> show e
+
+entitySpeed :: EntityType -> V3 Float
+entitySpeed e = case e of
+                    Zombie          -> V3 0.5 0.0 0.5
+                    Chicken         -> V3 0.75 0.5 0.75
+                    Exploder        -> V3 0.75 0.0 0.75
+                    TallCreepyThing -> V3 1.0 1.0 1.0
+
+entityBaseHealth :: EntityType -> Int
+entityBaseHealth e = case e of
+                         Zombie          -> 50
+                         Chicken         -> 25
+                         Exploder        -> 75
+                         TallCreepyThing -> 500
+
+mkEntity :: V3 Float -> EntityType -> Entity
+mkEntity loc typ = Entity { eLocation = loc
+                          , eHealth = entityBaseHealth typ
+                          , eType = typ
+                          }
+
+numBlocks :: Int
+numBlocks = 65536
+
+numEntities :: Int
+numEntities = 1000
+
+numChunks :: Int
+numChunks = 100
+              
+type BlockId = Word8
+
+-- chunks hold blockids, not actual blocks
+data Chunk = Chunk { cBlocks   :: !(V.Vector BlockId)
+                   , cEntities :: !(V.Vector Entity)
+                   , cLocation :: !(V3 Float)
+                   } deriving (Eq, Generic, Show)
+
+instance NFData Chunk
+
+mkChunk :: Int -> Chunk
+mkChunk p = Chunk (V.generate numBlocks fromIntegral)
+                  (V.generate numEntities newEntity)
+                  (V3 p' p' p')
+  where
+    p' = fromIntegral p
+    newEntity n =
+      let i = fromIntegral n
+      in  case n .&. 3 of
+              0 -> mkEntity (V3 i i i) Chicken
+              1 -> mkEntity (V3 i i i) Zombie
+              2 -> mkEntity (V3 i i i) Exploder
+              _ -> mkEntity (V3 i i i) TallCreepyThing
+
+processEntities :: V.Vector Entity -> V.Vector Entity
+processEntities = V.map updateEntityPosition
+    where
+        updateEntityPosition e = e { eLocation = eLocation e ^+^ entitySpeed (eType e) }
+
+loadWorld :: World
+loadWorld = World (VB.generate 256 mkBlock)
+                  (VB.generate 100 mkChunk)
+                  (V3 0 0 0)
+                  numChunks
+
+data World = World { wblocks  :: !(VB.Vector Block)
+                   , wchunks  :: !(VB.Vector Chunk)
+                   , wploc    :: !(V3 Float)
+                   , wcounter :: !Int
+                   } deriving (Generic)
+
+instance NFData World
+
+loop :: World -> World
+loop (World blocks chunks oldpp counter) = World blocks chunks' playerPosition counter'
+    where
+        playerPosition = oldpp ^+^ V3 0.1 0.0 0.0
+        (chunks', counter') = runState (traverse updateChunk chunks) counter
+        newId :: State Int Int
+        newId = do
+            x <- get
+            let !x' = x + 1
+            put x'
+            return x'
+        updateChunk :: Chunk -> State Int Chunk
+        updateChunk c = let !d = distance (cLocation c) playerPosition
+                        in  if d > 100
+                                then fmap mkChunk newId
+                                else return (c { cEntities = processEntities (cEntities c) })
+
+nloops :: World -> Int64 -> World
+nloops !w 0 = w
+nloops !w n = nloops (loop w) (n - 1)
+
+run :: IO ()
+run = do
+  putStrLn "NaiveGame3: Running version 3 with unboxed Entity Vector."
+  putStrLn "Loading World..."
+  start <- getTime MonotonicRaw
+  evaluate $ rnf loadWorld 
+  end <- getTime MonotonicRaw
+  putStrLn $ "FINISHED loading in "++
+             show (fromIntegral (toNanoSecs (diffTimeSpec end start)) / 1000.0)++" ms"
+  defaultMain [ bench "main loop" $ 
+                     Benchmarkable (void . evaluate . nloops loadWorld) ]
+
